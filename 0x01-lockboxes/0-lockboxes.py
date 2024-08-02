5288af6c38165273d031b979d603493da218dfd8
Peacechimamaka <peaceamaka111@gmail.com>
2024-08-03 00:58:38 +0100

    0x01-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = set()
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        if box not in opened:
+            opened.add(box)
+            for key in boxes[box]:
+                if key < n:
+                    stack.append(key)
+
+    return len(opened) == n
+
+# Test cases
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
+
