1f311dfcc0f802e8a1c34fbf7c30f964881fe83f
Peacechimamaka <peaceamaka111@gmail.com>
2024-08-03 01:04:24 +0100

    0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
+"""
+This module contains the function `canUnlockAll` that determines if all boxes in a list of boxes can be opened.
+
+Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+Box 0 is initially unlocked.
+
+Example Usage:
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
+"""
+
 def canUnlockAll(boxes):
-    '''A function on check boxes'''
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list of list of int): List where each index represents a box and contains a list of keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)
     opened = set()
     stack = [0]
