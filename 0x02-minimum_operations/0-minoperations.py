4c53ff9cdd2df088e3cfea024833940d9a29844e
Peacechimamaka <peaceamaka111@gmail.com>
2024-08-09 12:00:30 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 def minOperations(n):
     '''
-    Calculate the minimum number of operations.int: The minimum number
+    Calculate the minimum number of operations.
+
+    Args:
+        n (int): The target number of characters.
+
+    Returns:
+        int: The minimum number of operations required, or 0 if `n` is to 1.
     '''
 
-    operations = 0  # Initialize the number of operations performed.
-    factor = 2  # Start with the smallest prime factor.
+    operations = 0
+    factor = 2
 
     if n <= 1:
-        return 0  # If n is 1 or less, no operations are needed.
+        return 0
 
-    # Iterate until we reduce n to 1 by dividing it by its smallest factors.
     while n > 1:
-        # If n is divisible by the current factor, divthe factor
-        # and add the factor to the total operations count.
         if n % factor == 0:
             n = n / factor
             operations += factor
         else:
-            factor += 1  # Increment the factor to check.
-    return operations  # Return the total number of op.
+            factor += 1
+    return operations
